29100caa7c85ee01753d58b02990bbcee94d9af9
CharlieDeltaZA <calvin@diogo.co.za>
2017-05-23 18:31:53 +0200

    Prefix var

--- main.py
+++ main.py
@@ -3,29 +3,30 @@ import credentials
 
 client = discord.Client()
 
+prefix = '!'
+
 @client.event
 async def on_message(message):
    # Bot must not respond to itself
    if message.author == client.user:
       return
    # Our test message params
-   if message.content.startswith('!hello'):
-      msg = 'Hello {0.author.mention}'.format(message)
+   if message.content.startswith(prefix + 'hello'):                     # Greetings
+      msg = 'Hello {0.author.mention}!'.format(message)
       await client.send_message(message.channel, msg)
 
-   if message.content.startswith('!ping'):
+   if message.content.startswith(prefix + 'ping'):                      # C'mon. This must be here.
       msg = 'Pong!'
       await client.send_message(message.channel, msg)
 
-   if message.content.startswith('!bbb'):
-      # server = member.server
-      msg = 'Bish Bash Bosh'
+   if message.content.startswith(prefix + 'bbb'):                       # Why does this exist? I couldn't tell you really...
+      msg = 'Bish Bash Bosh!'
       await client.send_message(message.channel, msg, tts=True)
 
 # @client.event
 # async def on_message(message):
 
-@client.event
+@client.event                                                           # Welcome new members to server
 async def on_member_join(member):
    server = member.server
    fmt = 'Welcome {0.mention} to {1.name}!'
