1ea155b1b8f2c92beb91bf6e4053538045daa9ec
CharlieDeltaZA <calvin@diogo.co.za>
2017-05-23 16:26:03 +0200

    2nd message type test

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ async def on_message(message):
       msg = 'Hello {0.author.mention}'.format(message)
       await client.send_message(message.channel, msg)
 
+   if message.content.startswith('!ping'):
+      msg = 'Pong!'
+      await client.send_message(message.channel, msg)
+
+# @client.event
+# async def on_message(message):
+
+
 @client.event
 async def on_ready():
    print('Logged in as')
