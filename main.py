f71a220357f73de887640cf3084b972756170830
CharlieDeltaZA <calvin@diogo.co.za>
2017-05-23 17:40:35 +0200

    Memeber join str test

--- main.py
+++ main.py
@@ -20,6 +20,12 @@ async def on_message(message):
 # @client.event
 # async def on_message(message):
 
+@client.event
+async def on_member_join(member):
+   server = member.server
+   fmt = 'Welcome {0.mention} to {1.name}!'
+   await client.send_message(server, fmt.format(member, server))
+
 
 @client.event
 async def on_ready():
