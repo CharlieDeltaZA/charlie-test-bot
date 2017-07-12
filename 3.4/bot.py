8d842ca87ac08762147904db85d38fd80739a517
CharlieDeltaZA <calvin@diogo.co.za>
2017-07-12 12:08:22 +0200

    Py3.4 Tests

--- 3.4/bot.py
+++ 3.4/bot.py
@@ -10,35 +10,36 @@ import asyncio
 client = discord.Client()
 
 @client.event
-async def on_message(message):                                          # Indiv @events for every message command?
+@asyncio.coroutine
+   def on_message(message):                                          # Indiv @events for every message command?
    # Bot must not respond to itself
    if message.author == client.user:
       return
    # Our test message params
-   if message.content.startswith('!hello'):                     # Greetings
+   if message.content.startswith('*hello'):                     # Greetings
       msg = 'Hello {0.author.mention}!'.format(message)
-      await client.send_message(message.channel, msg)
+      yield from client.send_message(message.channel, msg)
 
-   if message.content.startswith('!ping'):                      # C'mon. This must be here.
+   if message.content.startswith('*ping'):                      # C'mon. This must be here.
       msg = 'Pong!'
-      await client.send_message(message.channel, msg)
+      yield from client.send_message(message.channel, msg)
 
-   if message.content.startswith('!bbb'):                       # Why does this exist? I couldn't tell you really...
+   if message.content.startswith('*bbb'):                       # Why does this exist? I couldn't tell you really...
       msg = 'Bish Bash Bosh!'
-      await client.send_message(message.channel, msg, tts=True)
+      yield from client.send_message(message.channel, msg, tts=True)
 
    if message.content == 'How do you feel, Bot?':						#FeelsBadMan
       emoji = '\N{UPSIDE-DOWN FACE}'                                                    # erm
-      await client.add_reaction(message, emoji)
+      yield from client.add_reaction(message, emoji)
 
-   if message.content.startswith('!help'):								# Send a PM to the user with a list of commands sourced elsewhere & formatted
+   if message.content.startswith('*help'):								# Send a PM to the user with a list of commands sourced elsewhere & formatted
       msg = commands.Help
-      await client.send_message(message.author, msg)
+      yield from client.send_message(message.author, msg)
 
-   if message.content.startswith('!lul'):
+   if message.content.startswith('*lul'):
    	msg_og = '<:NotLikeThis:314339829385265153>'						# Repeat what will be :lol: (lul?) 5 times, you know, for the lulz
    	msg = (msg_og + '\n') * 5
-   	await client.send_message(message.channel, msg)
+      yield from client.send_message(message.channel, msg)
 
    '''if message.content.startswith('James is a noob'):									# Wonder if this will work?
    	usr = client.get_all_members()
@@ -52,21 +53,23 @@ async def on_message(message):                                          # Indiv 
 #   if message.content.startswith('How do you feel, Bot?'):           # FUCKING SYNTAX GOD DAMNIT
 
 @client.event                                                           # Welcome new members to server
-async def on_member_join(member):
+@asyncio.coroutine
+   def on_member_join(member):
    server = member.server
    msg = 'Welcome {0.mention} to {1.name}!'
-   await client.send_message(server, msg.format(member, server))
+   yield from client.send_message(server, msg.format(member, server))
    
 
 @client.event                                                           # Debug text
-async def on_ready():
+@asyncio.coroutine
+   def on_ready():
    DateTime = datetime.datetime.now()
    print('Logged in as')
    print(client.user.name)
    print(client.user.id)
    print(DateTime.strftime("%Y-%m-%d %H:%M:%S"))                                                 # Debugging
    print('----------')
-   await client.change_presence(game=discord.Game(name='with code | !help'))   # Dis works 
+   yield from client.change_presence(game=discord.Game(name='with code | !help'))   # Dis works 
 
 
 client.run(credentials.Token)                                           # Run the client with the token
--- 3.4/commands.py
+++ 3.4/commands.py
@@ -1,9 +1,9 @@
 # A formatted list of commands etc
 
 Help = '''Commands: \n\n
-		!help : Sends this PM \n
-		!hello : Greets you \n 
-		!ping : Pong \n 
-		!bbb : CSGO Memes \n
-		!lul : Try it and find out \n\n 
+		*help : Sends this PM \n
+		*hello : Greets you \n 
+		*ping : Pong \n 
+		*bbb : CSGO Memes \n
+		*lul : Try it and find out \n\n 
 	Ask me how I\'m feeling : `How do you feel, Bot?`'''
